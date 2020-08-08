963da540b77d4d3e0735870721f8e3cbdf909d55
Sreehari Krishnakumar <sreehari-krishnakumar@users.noreply.github.com>
2020-08-08 13:31:33 +0530

    Update facialrec.py

--- facialrec.py
+++ facialrec.py
@@ -1,7 +1,4 @@
-import cv2,os,time
-import numpy as np
-import subprocess
-from PIL import Image
+import cv2
 recognizer = cv2.face.LBPHFaceRecognizer_create();
 cascadePath= cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml");
 recognizer.read('trainningData.yml')
@@ -9,28 +6,27 @@ cam = cv2.VideoCapture(0)
 fontface = cv2.FONT_HERSHEY_SIMPLEX
 fontscale = 1
 fontcolor = (255, 255, 255)
-i=1
 Id=0
 while(1):
     ret, facialrec = cam.read()
     gray=cv2.cvtColor(facialrec,cv2.COLOR_BGR2GRAY)
     faces=cascadePath.detectMultiScale(gray, 1.2,5)
-    for(x,y,w,h) in faces:
-        cv2.rectangle(facialrec,(x,y),(x+w,y+h),(225,0,0),2)
-        Id,conf = recognizer.predict(gray[y:y+h,x:x+w])
-        print ("id",Id)
-        if(conf<50):
-            if(Id==2):
-                Id="Username"
-        elif(conf>50):
-            Id="Unknown"
-        cv2.putText(facialrec, str(Id), (x,y+h), fontface, fontscale, fontcolor)
-        cv2.imshow("img",facialrec)
+    if(len(faces)):
+        for(x,y,w,h) in faces:
+            cv2.rectangle(facialrec,(x,y),(x+w,y+h),(225,0,0),2)
+            Id,conf = recognizer.predict(gray[y:y+h,x:x+w])
+            if(conf<95):
+                if(Id==ID_No.): 
+                    Id="NAME OF THE PERSON"
+            else:
+                Id="unknown"
+            cv2.putText(facialrec, str(Id), (x,y+h), fontface, fontscale, fontcolor)
+            cv2.imshow("img",facialrec)            
+    else:
+        cv2.imshow("img", facialrec)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cam.release()
-# and release the output
-# finally, close the window
 cv2.destroyAllWindows()
 cv2.waitKey(1)
